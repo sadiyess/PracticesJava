1b91db2c40043762791a7b31d0ba8c053550df5a
Şadiye Şahin <sadiye_sahn@hotmail.com>
2024-03-17 15:55:09 +0300

    hipotenüsHesaplama

--- src/hipotenüsHesaplama/hipotenüsHesaplama.java
+++ src/hipotenüsHesaplama/hipotenüsHesaplama.java
@@ -1,2 +1,23 @@
-package hipotenüsHesaplama;public class hipotenüsHesaplama {
+package hipotenüsHesaplama;
+
+import java.util.Scanner;
+
+public class hipotenüsHesaplama {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        double c;
+
+        a= input.nextInt();
+        System.out.println(" Lütfen 1.Kenarı giriniz ");
+
+        b= input.nextInt();
+        System.out.println(" Lütfen 2.Kenarı giriniz ");
+
+        c = Math.sqrt((a*a)+(b*b));
+        System.out.println(c);
+
+
+    }
 }
